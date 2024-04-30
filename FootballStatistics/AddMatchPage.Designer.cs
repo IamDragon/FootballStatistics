dd9b31ae7e0d6d812e238e6ad454336153a0e961
Bloodbone9829 <marcomatejic@gmail.com>
2024-04-30 15:47:05 +0200

    AddMatch inProgress

--- FootballStatistics/AddMatchPage.Designer.cs
+++ FootballStatistics/AddMatchPage.Designer.cs
@@ -28,12 +28,245 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.teamA1panel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.teamB1panel = new System.Windows.Forms.Panel();
+            this.label12 = new System.Windows.Forms.Label();
+            this.teamAWinCheckbox = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.teamA2panel = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.teamBWinCheckbox = new System.Windows.Forms.CheckBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.teamA1panel.SuspendLayout();
+            this.teamB1panel.SuspendLayout();
+            this.teamA2panel.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // teamA1panel
+            // 
+            this.teamA1panel.Controls.Add(this.teamA2panel);
+            this.teamA1panel.Controls.Add(this.label5);
+            this.teamA1panel.Location = new System.Drawing.Point(37, 172);
+            this.teamA1panel.Name = "teamA1panel";
+            this.teamA1panel.Size = new System.Drawing.Size(214, 225);
+            this.teamA1panel.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(213, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Add Match";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(107, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Team A";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(359, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Team B";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 11);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Team ID";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 19);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "MVP ID";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 49);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 13);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Goals";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 78);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(34, 13);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "Votes";
+            // 
+            // teamB1panel
+            // 
+            this.teamB1panel.Controls.Add(this.panel1);
+            this.teamB1panel.Controls.Add(this.label12);
+            this.teamB1panel.Location = new System.Drawing.Point(270, 172);
+            this.teamB1panel.Name = "teamB1panel";
+            this.teamB1panel.Size = new System.Drawing.Size(214, 225);
+            this.teamB1panel.TabIndex = 3;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(12, 11);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(48, 13);
+            this.label12.TabIndex = 3;
+            this.label12.Text = "Team ID";
+            // 
+            // teamAWinCheckbox
+            // 
+            this.teamAWinCheckbox.AutoSize = true;
+            this.teamAWinCheckbox.Location = new System.Drawing.Point(15, 108);
+            this.teamAWinCheckbox.Name = "teamAWinCheckbox";
+            this.teamAWinCheckbox.Size = new System.Drawing.Size(60, 17);
+            this.teamAWinCheckbox.TabIndex = 7;
+            this.teamAWinCheckbox.Text = "Winner";
+            this.teamAWinCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(203, 98);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(107, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Upcoming Match";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // teamA2panel
+            // 
+            this.teamA2panel.Controls.Add(this.label6);
+            this.teamA2panel.Controls.Add(this.teamAWinCheckbox);
+            this.teamA2panel.Controls.Add(this.label7);
+            this.teamA2panel.Controls.Add(this.label8);
+            this.teamA2panel.Location = new System.Drawing.Point(0, 39);
+            this.teamA2panel.Name = "teamA2panel";
+            this.teamA2panel.Size = new System.Drawing.Size(214, 186);
+            this.teamA2panel.TabIndex = 8;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.teamBWinCheckbox);
+            this.panel1.Controls.Add(this.label13);
+            this.panel1.Controls.Add(this.label14);
+            this.panel1.Location = new System.Drawing.Point(0, 39);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(214, 186);
+            this.panel1.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "MVP ID";
+            // 
+            // teamBWinCheckbox
+            // 
+            this.teamBWinCheckbox.AutoSize = true;
+            this.teamBWinCheckbox.Location = new System.Drawing.Point(15, 108);
+            this.teamBWinCheckbox.Name = "teamBWinCheckbox";
+            this.teamBWinCheckbox.Size = new System.Drawing.Size(60, 17);
+            this.teamBWinCheckbox.TabIndex = 7;
+            this.teamBWinCheckbox.Text = "Winner";
+            this.teamBWinCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(12, 49);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(34, 13);
+            this.label13.TabIndex = 5;
+            this.label13.Text = "Goals";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(12, 78);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(34, 13);
+            this.label14.TabIndex = 6;
+            this.label14.Text = "Votes";
+            // 
+            // AddMatchPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(563, 478);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.teamB1panel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.teamA1panel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Name = "AddMatchPage";
             this.Text = "AddMatchPage";
+            this.teamA1panel.ResumeLayout(false);
+            this.teamA1panel.PerformLayout();
+            this.teamB1panel.ResumeLayout(false);
+            this.teamB1panel.PerformLayout();
+            this.teamA2panel.ResumeLayout(false);
+            this.teamA2panel.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel teamA1panel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel teamB1panel;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.CheckBox teamAWinCheckbox;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Panel teamA2panel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox teamBWinCheckbox;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
     }
 }
